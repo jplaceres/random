c59a18b0f4379ecb97720d7a79304d115bd2067c
jplaceres <33101350+jplaceres@users.noreply.github.com>
2019-09-18 15:20:00 -0400

    submitted another file

---
+++ Challenge2.py
@@ -0,0 +1,25 @@
+"""
+You are planning on having a surprise party for Andrew Crosswait.
+  • People will be attending and bringing food that serves a different number of people.
+  
+  • You need to create a Python program to keep track of who’s coming, what they are bringing, and how many people you will be able to serve.
+  
+  • Create a program that uses a list to keep track of all of the required information.
+  
+  • Here’s the data that you will load into your list: 
+  Frank, pizza, 10, Mary, wings, 8, Bob, cookies, 15, Lisa, dip, 20, 
+  Mark, brownies, 12, Ann, burgers, 7, Henry, mints, 30, Ruth, hotdogs, 22
+  
+  • After entering the data, answer the following questions:
+    – How many people attending?
+    – What foods will be there?
+    – How many people can be fed?
+"""
+
+# Add your own list here!
+
+# Find out how many people will be attending?
+
+# Find out What foods will be served.
+
+# Find out how many people can be fed.
