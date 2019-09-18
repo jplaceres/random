9d1fddd6413daf6379d43dc73f4108d3d0f26a20
jplaceres <33101350+jplaceres@users.noreply.github.com>
2019-09-18 15:12:56 -0400

    created Challenge1.py
    this is great!

---
+++ Challenge1.py
@@ -0,0 +1,21 @@
+#
+# Decription: This program will analize the upper and lower case content of
+# a given string.
+#
+
+
+
+sampleString = "We the People of the United States, in Order+ to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America."
+
+"""
+## 1) Start by counting the number of upper, lower, and other characters inthe provided string. Print your results.
+"""
+
+# 2) Next, invert the case of all of the text in the sample string. Print the resulting string.
+
+# 3) Place all vowels in one list. 
+
+# 4) Place all consonant in another list. 
+
+# 5) Find the decimal value of each character and place all characters that are multiples of 3 in another list.
+
